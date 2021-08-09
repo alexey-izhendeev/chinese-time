b7f4bf389de49c39c0e5a227525d395e3b952208
Alexey Izhendeev <takedo19@gmail.com>
2021-08-09 16:36:13 +0700

    Made autocomplete of form coordinates

--- chinese/chinese/settings.py
+++ chinese/chinese/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+#from dotenv import load_dotenv
 # from passwords import sec_key_pas
 #  from chinese.passwords import sec_key_pas
 
